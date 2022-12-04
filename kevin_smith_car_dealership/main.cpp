29ae33e8e9ab6b5395494476c97af579f8463d5d
Kevin Smith <kevin341s@yahoo.com>
2022-12-04 18:17:05 -0500

    Initial Commit

---
+++ kevin_smith_car_dealership/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  kevin_smith_car_dealership
+//
+//  Created by Kevin Smith on 12/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
