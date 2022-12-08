fc882f882a1d33d41ce7d25abcb7910ed247bdc8
Kevin Smith <kevin341s@yahoo.com>
2022-12-07 21:28:10 -0500

    Added new car class
    added and set up header and c++ file

---
+++ kevin_smith_car_dealership/newCar.cpp
@@ -0,0 +1,8 @@
+//
+//  newCar.cpp
+//  kevin_smith_car_dealership
+//
+//  Created by Kevin Smith on 12/7/22.
+//
+
+#include "newCar.h"
---
+++ kevin_smith_car_dealership/newCar.h
@@ -0,0 +1,15 @@
+//
+//  newCar.hpp
+//  kevin_smith_car_dealership
+//
+//  Created by Kevin Smith on 12/7/22.
+//
+
+#ifndef newCar_h
+#define newCar_h
+#include <stdio.h>
+#include "Car.h"
+
+
+
+#endif /* newCar_h */
