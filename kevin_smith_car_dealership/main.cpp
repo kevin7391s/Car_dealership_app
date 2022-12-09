ed3bb9f5b81fa3c0b8577e7bb9a80f07d63a34e1
Kevin Smith <kevin341s@yahoo.com>
2022-12-08 20:52:05 -0500

    added file
    and tested in main

--- kevin_smith_car_dealership/main.cpp
+++ kevin_smith_car_dealership/main.cpp
@@ -7,17 +7,30 @@
 
 #include <iostream>
 #include "Car.h"
+#include <fstream>
 using namespace std;
 
 int main() {
     Car car1;
     
+    ofstream MyFile;
+    MyFile.open("File.txt");
     
+
+    // Write to the file
+    MyFile << "Files can be tricky, but it is fun enough!\n";
+
+    // Close the file
+    MyFile.close();
   
     cout<<car1.getYear()<<endl;
     car1.setYear(1999);
-    cout<<car1.getYear()<<endl;
-
+    MyFile <<car1.getYear();
+    
+    
+    //MyFile <<"Hellowordl";
+  
+    
     
     return 0;
 }
